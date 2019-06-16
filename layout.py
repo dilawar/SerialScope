d1fe1a2794e33eece8343ff2679c791dfb48a574
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-06-16 16:00:07 +0530

    DrawLine is not working.

--- layout.py
+++ layout.py
@@ -9,19 +9,30 @@ __maintainer__       = "Dilawar Singh"
 __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
-import sys
 import PySimpleGUI as sg  
 import tkinter as TK
 import config
 import PIL.ImageTk 
 import PIL.Image
 import time
+import random
 
-# tabbed layout. One tab of interative window. Other for returned artifacts.
-artifactTab = sg.Tab('Data', [[sg.Canvas(size=(config.w_*2//3,config.h_),
-    key='data')]])
-currentTab = sg.Tab('Live', [[sg.Canvas(size=(config.w_*2//3,config.h_),
-    key='live')]])
+W = config.w_
+H = config.h_
+
+# Two graphs. One for channel 1 and other for channel 2.
+graphChannel = [
+        [sg.Graph(canvas_size=(W*2//3, H//2), graph_bottom_left=(0,-255),
+            graph_top_right=(1000, 255), background_color='white', key='channelA')],
+        [sg.Graph(canvas_size=(W*2//3, H//2), graph_bottom_left=(0,0),
+            graph_top_right=(1000, 255), background_color='white', key='channelB')],
+        ]
+
+currentTab = sg.Tab('Live', graphChannel)
+artifactTab = sg.Tab('Data'
+        , [ ]
+        #  , [[sg.Canvas(size=(config.w_*2//3,config.h_), key='data')]]
+        )
 
 # ------ Column Definition ------ #
 column1 = [
@@ -37,12 +48,18 @@ widgets =  sg.Frame('Labelled Group',[[
     sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=10),
     sg.Column(column1, background_color='lightblue')]])
 
+
 layout = [ [sg.TabGroup([[currentTab, artifactTab]]), widgets]
         , [sg.Submit('Launch'), sg.Exit('Quit')] ]
 
 # We want it global. Otherwise garbage collected will destroy the images.
 images_ = { }
+mainWindow = sg.Window('NeuroScope').Layout(layout).Finalize()
 
+chAGraph_ = mainWindow.FindElement("channelA")
+chBGraph_ = mainWindow.FindElement("channelB")
+
+# helpful function.
 def draw_canvas(canvas, imgs):
     global images_
     if not isinstance(imgs, list):
@@ -52,4 +69,26 @@ def draw_canvas(canvas, imgs):
         canvas.TKCanvas.create_image(0, 0, anchor=TK.NW ,image=im)
         time.sleep(1)
 
-mainWindow = sg.Window('NeuroScope').Layout(layout).Finalize()
+def update_channel_window(data):
+    global chAGraph_, chBGraph_
+    y = random.randint(0, 100) * 1.0
+    #  chAGraph_.Erase()
+    for (t0, a0, b0), (t1, a1, b1) in zip(data, data[1:]):
+        print(y, end = ' ')
+        x = chAGraph_.DrawLine((1.0,1.0), (10.0,y), color='red')
+        assert x
+        print( chAGraph_.Size, chAGraph_.Visible )
+        #  print( f"Drawing {(t0, a0)} to {(t1, a1)}" )
+        #  chAGraph_.DrawLine( (int(t0*1000), a0), (int(t1*1000), a1), color='black')
+        #  chBGraph_.DrawLine( (t0, b0), (t1, b1), color='black')
+    #  chAGraph_.update()
+    #  chBGraph_.update()
+
+def main():
+    global mainWindow
+    while True:
+        data = [(1,1,1), (1,2,2), (1,3,2)]
+        update_channel_window(data)
+
+if __name__ == '__main__':
+    main()
