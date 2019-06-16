88f7024098b68196bbf984e9cff3716abf301735
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-06-16 16:48:52 +0530

    Removed SoftOscillopscope.

--- SoftScope/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-PY=$(which python3)
-$PY ./SocketPlot-Test.py &
-TEST_PID=$!
-$PY ./test_run.py 
-kill $TEST_PID
--- SoftScope/test_run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from SoftOscilloscope import SocketClientPlot
-plot = SocketClientPlot('localhost', 5000)
-plot.start()
