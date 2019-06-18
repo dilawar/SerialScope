9dca125433846c15d5df637545e94ffa8b86acb2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-06-18 17:02:54 +0530

    setup.py file for release.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import os
-import sys
-from setuptools import setup
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
 
 with open("README.md") as f:
     readme = f.read()
@@ -10,12 +10,12 @@ setup(
     version = "0.0.1",
     description = "A oscilloscope for using arduino board.",
     long_description = readme,
-    packages = ["ArduinoScope" ],
+    #  packages = ["ArduinoScope" ],
+    packages = find_packages(),
     package_data = {},
     install_requires = [ ],
     author = "Dilawar Singh",
     author_email = "dilawars@ncbs.res.in",
-    url = "http://github.com/dilawar/",
+    url = "http://github.com/dilawar/ArduinoScope",
     license='GPL',
-    classifiers=classifiers,
 )
