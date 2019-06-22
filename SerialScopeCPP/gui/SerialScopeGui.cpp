6ae243c3f649a5140269943b3ae4d6600390035d
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-06-23 00:02:26 +0530

    Updated a bit more.

---
+++ SerialScopeCPP/gui/SerialScopeGui.cpp
@@ -0,0 +1,21 @@
+// =====================================================================================
+//
+//       Filename:  SerialScopeGui.cpp
+//
+//    Description:  GUI.
+//
+//        Version:  1.0
+//        Created:  06/22/2019 11:42:56 PM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Dilawar Singh (), dilawar.s.rajput@gmail.com
+//   Organization:  NCBS Bangalore
+//
+// =====================================================================================
+
+#include <QWidget>
+#include "SerialScopeGui.h"
+
+SerialScopeGui::SerialScopeGui(QWidget *parent): QWidget(parent)
+{ }
