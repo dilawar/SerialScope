49a5a56df81d4117d03d780795787c602edb8c87
Dilawar Singh <dilawars@ncbs.res.in>
2019-06-22 21:47:24 +0530

    bumped version to 0.0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as f:
 
 setup(
     name = "SerialScope",
-    version = "0.0.6",
+    version = "0.0.7",
     description = "A serial-port based oscilloscope",
     long_description = readme,
     long_description_content_type='text/markdown',
