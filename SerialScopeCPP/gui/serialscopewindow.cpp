e94f84dbbf21c17d9213b6ba6d0040b4ab78a269
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-06-23 15:38:00 +0530

    compiles fine.

---
+++ SerialScopeCPP/gui/serialscopewindow.cpp
@@ -0,0 +1,16 @@
+/***
+ *    Description:  Main window gui.
+ *
+ *        Created:  2019-06-23
+
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *        License:  GPLv3
+ */
+
+#include "serialscopewindow.h"
+
+SerialScopeWindow::SerialScopeWindow(QWidget *parent) : QMainWindow(parent)
+{
+
+}
